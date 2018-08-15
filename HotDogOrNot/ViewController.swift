41af2cf716b6d29057e3a26158ca577d74b718c4
yinduo <liyinduo@gmail.com>
2018-08-15 16:46:47 +0800

    Initial Commit

---
+++ HotDogOrNot/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HotDogOrNot
+//
+//  Created by LIYINDUO on 15/8/18.
+//  Copyright © 2018 LIYINDUO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
